3852137a5fb626f0d32f492e162b6e558d2c1e45
JalmariPalmari <puura.janne@gmail.com>
2020-01-28 01:08:48 +0200

    1

--- app-02-sessions-mongo.js
+++ app-02-sessions-mongo.js
@@ -126,7 +126,7 @@ app.use((req, res, next) => {
 
 //Shutdown server CTRL + C in terminal
 
-const mongoose_url = 'mongodb+srv://db-user:1KRO2OhneATkq0Ke@cluster0-soknu.mongodb.net/test?retryWrites=true&w=majority';
+const mongoose_url = 'mmongodb+srv://dbJan:<Admin>@cluster0-y7oug.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(mongoose_url, {
     useUnifiedTopology: true,
